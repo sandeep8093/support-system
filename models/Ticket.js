ce4b8bdf8f0303dd42248e2137e613a423f52096
sandeep8093 <nayaksandeep676@gmail.com>
2024-01-25 19:44:07 +0530

    maij

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,42 +1,45 @@
 const mongoose = require('mongoose');
+
 const ticketSchema = mongoose.Schema(
   {
-    topic: { 
-        type: String, 
-        required: true 
+    topic: {
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     severity: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
+      enum: ['High', 'Medium', 'Low'],
     },
-    type: { 
-        type: String, 
-        required: true 
+    type: {
+      type: String,
+      required: true,
+      enum: ['Type1', 'Type2', 'Type3'],
     },
     assignedTo: {
-        type: String, 
-        required: false,
-      },
-    status: { 
-        type: String, 
-        required: true,
-        default:"New" 
+      type: String,
+      required: false,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['New', 'Assigned', 'Resolved'],
+      default: 'New',
     },
     dateCreated: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now,
     },
     resolvedOn: {
-        type: Date,
-        required: false
+      type: Date,
+      required: false,
     },
   },
-  { timestamps: true },
-  { minimize: false }
+  { timestamps: true, minimize: false }
 );
 
 module.exports = mongoose.model('Ticket', ticketSchema);
